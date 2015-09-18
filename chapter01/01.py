0c30ca0b61707345c9351c404fe0fc41251603ed
Shunsuke KITADA <septemtrio.ager@gmail.com>
2015-09-18 20:13:05 +0900

    import文を追加

---
+++ chapter01/01.py
@@ -0,0 +1,10 @@
+# ======================================
+#
+# 1章コード写経
+#
+# ======================================
+
+import os
+from utils import DATA_DIR, CHART_DIR
+import scipy as sp
+import matplotlib.pyplot as plt
